58e7f212eec1e0edcc7c3080e9f93020080464b1
nkashyap <nkashyap.anand@gmail.com>
2022-11-20 00:51:11 +0530

    maximum path sum

--- tree/Binary Tree Maximum Path Sum.cpp
+++ tree/Binary Tree Maximum Path Sum.cpp
@@ -13,9 +13,11 @@ public:
         if (!node)
             return 0;
 
+        // ignore negative nodes
         int l = max(0, dfsHeight(node->left, sum));
         int r = max(0, dfsHeight(node->right, sum));
 
+        // sum at every node
         sum = max(sum, l + r + node->val);
 
         return (node->val) + max(l, r);
