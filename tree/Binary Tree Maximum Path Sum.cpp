cc0a4e0f6e4460902949ca6e759e2091d8e9a93d
nkashyap <nkashyap.anand@gmail.com>
2022-11-20 00:49:50 +0530

    dfs on binary tree for maximum sum

---
+++ tree/Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int maxPathSum(TreeNode *root)
+    {
+        int sum = INT_MIN;
+        dfsHeight(root, sum);
+        return sum;
+    }
+
+    int dfsHeight(TreeNode *node, int &sum)
+    {
+        if (!node)
+            return 0;
+
+        int l = max(0, dfsHeight(node->left, sum));
+        int r = max(0, dfsHeight(node->right, sum));
+
+        sum = max(sum, l + r + node->val);
+
+        return (node->val) + max(l, r);
+    }
+};
